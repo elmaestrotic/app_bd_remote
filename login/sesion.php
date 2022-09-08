07135267f22181054ff7417a0d29bcc3a9018129
elmaestrotic <45459513+elmaestrotic@users.noreply.github.com>
2022-09-08 10:22:25 -0500

    Folder con los webservices
    Esta carpeta contiene los archivos de php para iniciar sesión: login.php y para registrar ususarios: registrar.php
    archivos necesarios para la conexión remota de la app en android usando la librería volley

---
+++ login/sesion.php
@@ -0,0 +1,44 @@
+<?PHP
+$hostname="localhost";
+$database="test";
+$username="root";
+$password="";
+
+$json=array();
+	if(isset($_GET["user"]) && isset($_GET["pwd"])){
+		$user=$_GET['user'];
+		$pwd=$_GET['pwd'];
+		
+		$conexion=mysqli_connect($hostname,$username,$password,$database);
+		
+		$consulta="SELECT user, pwd, names FROM users WHERE user= '{$user}' AND pwd = '{$pwd}'";
+		$resultado=mysqli_query($conexion,$consulta);
+
+		if($consulta){
+		
+			if($reg=mysqli_fetch_array($resultado)){
+				$json['datos'][]=$reg;
+			}
+			mysqli_close($conexion);
+			echo json_encode($json);
+		}
+
+
+
+		else{
+			$results["user"]='';
+			$results["pwd"]='';
+			$results["names"]='';
+			$json['datos'][]=$results;
+			echo json_encode($json);
+		}
+		
+	}
+	else{
+		   	$results["user"]='';
+			$results["pwd"]='';
+			$results["names"]='';
+			$json['datos'][]=$results;
+			echo json_encode($json);
+		}
+?>
